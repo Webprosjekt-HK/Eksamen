24b38af943ce6432b7c3e807753f324c01617f8a
gabeogard <gaog001@student.kristiania.no>
2021-05-24 12:31:44 +0200

    Ansattliste kommer opp på avdelingssiden

---
+++ js/modules/MakeProfileBanner.js
@@ -0,0 +1,19 @@
+export default class MakeProfileBanner {
+
+    apply = (employeeNum, employeeName, role, phoneNumber, emailAdress) => {
+        console.log("Tester");
+        let html = `
+            <div class="column is-4">
+                <img src="images/ansatt_${employeeNum}.png" width="150" height="150">
+                <h2>${employeeName}</h2>
+                <p>${role}</p>
+                <p>${phoneNumber}</p>
+                <p>${emailAdress}</p>
+            </div>
+        `;
+
+        document.getElementById("employees").innerHTML += html;
+        
+    };
+
+}
