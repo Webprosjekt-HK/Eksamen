61456882a2a1aae65db784b28982bac06383d457
gabeogard <gaog001@student.kristiania.no>
2021-05-25 09:43:37 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/Webprosjekt-HK/Eksamen into main""
    This reverts commit d55406aad0743284afaac70010fda6c7cd3ce012.

--- js/modules/makeCalendar.js
+++ js/modules/makeCalendar.js
@@ -1,13 +1,7 @@
 const makeCalendar = (element, options, schedules) => {
     const calendar = new tui.Calendar(element, options);
-
-    // Mal for å lage vakter:
-    // console.log(new Date().toISOString());
     calendar.clear();
     calendar.createSchedules(schedules);
-
-    let schedule = calendar.getSchedule("3", "1");
-    console.log(schedule);
     return calendar;
 };
 
--- js/pages/profile.js
+++ js/pages/profile.js
@@ -1,19 +1,14 @@
 import MakeTile from "/js/modules/MakeTile.js";
 import makeCalendar from "/js/modules/MakeCalendar.js";
 import ShiftCollection from "/js/classes/ShiftCollection.js";
-import EmployeeCollection from "/js/classes/EmployeeCollection.js";
 
 const profile = (() => {
     const shiftCollection = new ShiftCollection();
     const shifts = shiftCollection.fetchShifts();
-    const employeeCollection = new EmployeeCollection();
 
     // The container in the index-file
     const mainElement = document.getElementById("main");
-    // Container to hold the cards on the profile-page
-    const body = document.createElement("div");
-    body.classList.add("container", "columns", "is-multiline");
-    body.id = "main-body";
+
     const calendarOptions = {
         defaultView: "month",
         scheduleView: true, // Can be also ['allday', 'time']
@@ -52,27 +47,75 @@ const profile = (() => {
         },
         month: {
             daynames: ["Søn", "Man", "Tir", "Ons", "Tor", "Fre", "Lør"],
-            startDayOfWeek: 0,
-            narrowWeekend: true,
+            startDayOfWeek: 1,
+            narrowWeekend: false,
         },
         week: {
             daynames: ["Søn", "Man", "Tir", "Ons", "Tor", "Fre", "Lør"],
-            startDayOfWeek: 0,
-            narrowWeekend: true,
+            startDayOfWeek: 1,
+            narrowWeekend: false,
         },
     };
 
     const init = (userObject) => {
-        // Sets the title for the page
-        body.innerHTML = `<h1 class="header column is-12 title">
-            ${userObject.firstName + " " + userObject.lastName}
-        </h1>`;
-        mainElement.innerHTML = "";
+        mainElement.innerHTML = `
+            <div id="outer-shift-container" class="column is-9">
+                <div class="columns is-multiline">
+                    <div class="column is-12">
+                        <h1 class="title">${
+                            userObject.firstName + " " + userObject.lastName
+                        }</h1>
+                    </div>
 
-        // Container for the calendar
-        const calendarDiv = document.createElement("div");
-        calendarDiv.id = "calendar";
-        calendarDiv.classList.add("column", "is-10");
+                    <div class="column is-12">
+                        <div class="columns is-multiline" id="work-info"></div>
+                    </div>
+                    <div class="column is-12" id="outer-calendar">
+                            <div id="calendar-menu">
+                                <button id="prev-btn" class="button">Previous</button>
+                                <button id="next-btn" class="button">Next</button>
+                            </div>
+                        <div class="container" id="calendar"></div>
+                    </div>
+                </div>
+            </div>
+            <div id="profile-info" class="column is-3 is-vcentered">
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-3by3">
+                            <img
+                                src="${userObject.pictureUrl}"
+                                alt="Placeholder image"
+                            />
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-4">
+                                    ${
+                                        userObject.firstName +
+                                        " " +
+                                        userObject.lastName
+                                    }
+                                </p>
+                                <p class="subtitle is-6">${userObject.role}</p>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <p class="italic">Fødselsdato:<br />FIKS MEG</p>
+                            <p class="italic">Telefon:<br />${
+                                userObject.phoneNumber
+                            }</p>
+                            <p class="italic">E-post: <br />FIKS MEG</p>
+                            <p class="italic">Adresse:<br />${
+                                userObject.address
+                            }</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
 
         // Get the shifts from logged in user, and format them for
         // further use in the calendar. Should probably move this
@@ -92,14 +135,18 @@ const profile = (() => {
                 });
             });
 
-        mainElement.append(body);
-        mainElement.append(calendarDiv);
         // Gets the object reference to the calendar back from the
         // function-call. Might get usefull
-        const calendar = makeCalendar(calendarDiv, calendarOptions, schedules);
+        const calendar = makeCalendar(
+            document.getElementById("calendar"),
+            calendarOptions,
+            schedules
+        );
+        document.getElementById("prev-btn").onclick = () => calendar.prev();
+        document.getElementById("next-btn").onclick = () => calendar.next();
 
         // Legg til tiles
-        const makeTile = new MakeTile("main-body");
+        const makeTile = new MakeTile("work-info");
         const nextShift = shiftCollection.getNextShiftByUserId(userObject.id);
         const hoursWorked = shiftCollection.getHoursWorkedByUserId(
             userObject.id
@@ -108,27 +155,33 @@ const profile = (() => {
         const previousMonth = new Date();
         previousMonth.setMonth(today.getMonth() - 1);
         const lastPaycheck = today.getDate() >= 15 ? today : previousMonth;
-
+        lastPaycheck.setDate(15);
         console.log(lastPaycheck);
         makeTile.apply(
-            "Ikon",
+            '<img src="/images/icons/id-card.svg" alt="id card icon"/>',
             "Neste vakt",
             nextShift.toLocaleString("nb-NO", {
                 weekday: "long",
                 month: "long",
                 day: "numeric",
             }),
-            "is-3"
+            "is-4"
         );
-        makeTile.apply("Ikon", "Timer jobbet", hoursWorked, "is-3");
         makeTile.apply(
-            "Ikon",
+            '<img src="/images/icons/clock.svg" alt="clock icon"/>',
+            "Timer jobbet",
+            hoursWorked,
+            "is-4"
+        );
+        makeTile.apply(
+            '<img src="/images/icons/paycheck.svg" alt="paycheck icon"/>',
             "Siste lønnsslipp",
-            "15. " +
-                lastPaycheck.toLocaleString("nb-NO", { month: "long" }) +
-                "." +
-                today.getFullYear(),
-            "is-3"
+            lastPaycheck.toLocaleString("nb-NO", {
+                weekday: "long",
+                day: "numeric",
+                month: "long",
+            }),
+            "is-4"
         );
     };
     return { init };
