16fdb224bf87a6eae911b98ec19333b6eb2e0a29
Christian Gregersen <chgr007@student.kristiania.no>
2021-05-25 15:38:35 +0200

    henter inn data til meny ved oppdatering

--- js/pages/shiftOverview.js
+++ js/pages/shiftOverview.js
@@ -9,13 +9,9 @@ const shiftOverview = ((state) => {
     const calendarOption = {
         defaultView: "week",
         taskView: ["task"],
-        useCreationPopup: true,
-        useDetailPopup: true,
+        useCreationPopup: false,
+        useDetailPopup: false,
         template: {
-            popupSave: (schedule) => {
-                console.log(schedule);
-                return "save";
-            },
             milestone: function (schedule) {
                 return (
                     '<span style="color:red;"><i class="fa fa-flag"></i> ' +
@@ -73,6 +69,109 @@ const shiftOverview = ((state) => {
             <div class="column is-2"></div>
         `;
     }
+    function generatePopupMenu(users, schedule, update) {
+        const containerElement = document.createElement("div");
+
+        const htmlContent = `
+            <nav class="panel is-primary">
+                
+                <p class="panel-heading">Legg til vakt</p>
+                <div class="panel-block">
+                    <div class="control has-icons-left">
+                        <div class="select">
+                            <select>
+                                ${users.map((e) => {
+                                    return (
+                                        '<option value="' +
+                                        e.id +
+                                        '">' +
+                                        e.firstName +
+                                        " " +
+                                        e.lastName +
+                                        "</option>"
+                                    );
+                                })}
+                            </select>
+                        </div>
+                        <span class="icon is-left">
+                            <i class="fas fa-search" aria-hidden="true"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="panel-block">
+                    <div class="control has-icons-left">
+                        <div
+                            class="
+                            tui-datepicker-input
+                            tui-datetime-input
+                            tui-has-focus
+                        "
+                        >
+                            <input
+                                type="text"
+                                id="datepicker-start-target"
+                                aria-label="Date-Time"
+                            />
+                            <span class="tui-ico-date"></span>
+                        </div>
+                        <div id="datepicker-start" class="control"></div>
+                    </div>
+                </div>
+                <div class="panel-block">
+                    <div class="control has-icons-left">
+                        <div
+                            class="
+                            tui-datepicker-input
+                            tui-datetime-input
+                            tui-has-focus
+                        "
+                        >
+                            <input
+                                type="text"
+                                id="datepicker-end-target"
+                                aria-label="Date-Time"
+                            />
+                            <span class="tui-ico-date"></span>
+                        </div>
+                        <div id="datepicker-end" class="control"></div>
+                    </div>
+                </div>
+                <div class="panel-block">
+                    <div class="control has-icons-left">
+                        <button class="button is-primary">Legg til</button>
+                    </div>
+                </div>
+            </nav>
+        `;
+
+        containerElement.innerHTML = htmlContent;
+        const picker1 = new tui.DatePicker(
+            containerElement.querySelector("#datepicker-start"),
+            {
+                input: {
+                    element: containerElement.querySelector(
+                        "#datepicker-start-target"
+                    ),
+                },
+            }
+        );
+        const picker2 = new tui.DatePicker(
+            containerElement.querySelector("#datepicker-end"),
+            {
+                input: {
+                    element: containerElement.querySelector(
+                        "#datepicker-end-target"
+                    ),
+                },
+            }
+        );
+        if (update) {
+            picker1.setDate(new Date(schedule.schedule.start));
+            picker2.setDate(new Date(schedule.schedule.end));
+            containerElement.querySelector(".button").innerHTML = "Oppdater";
+        }
+        return containerElement;
+    }
     const init = (state) => {
         generateScaffold(state);
         const shifts = shiftCollection.fetchShifts();
@@ -83,6 +182,7 @@ const shiftOverview = ((state) => {
             );
             schedules.push({
                 id: shift.id,
+                employeeID: shift.employeeID,
                 calendarId: "1",
                 title: userObject.firstName + " " + userObject.lastName,
                 category: "time",
@@ -98,6 +198,36 @@ const shiftOverview = ((state) => {
             calendarOption,
             schedules
         );
+        calendar.on({
+            clickSchedule: function (e) {
+                console.log("clickSchedule", e);
+                const popupBar = generatePopupMenu(
+                    employeeCollection.fetchEmployees(),
+                    e,
+                    true
+                );
+                mainContainer.append(popupBar);
+            },
+            beforeCreateSchedule: function (e) {
+                console.log("beforeCreateSchedule", e);
+                // open a creation popup
+            },
+            beforeUpdateSchedule: function (e) {
+                console.log("beforeUpdateSchedule", e);
+                e.schedule.start = e.start;
+                e.schedule.end = e.end;
+
+                cal.updateSchedule(
+                    e.schedule.id,
+                    e.schedule.calendarId,
+                    e.schedule
+                );
+            },
+            beforeDeleteSchedule: function (e) {
+                console.log("beforeDeleteSchedule", e);
+                cal.deleteSchedule(e.schedule.id, e.schedule.calendarId);
+            },
+        });
     };
     return { init };
 })();
