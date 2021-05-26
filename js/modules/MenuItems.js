7035e59c27cac89f4e6ece304e4619222e8ad1f2
gabeogard <71007034+gabeogard@users.noreply.github.com>
2021-05-26 16:30:01 +0200

    Meny nesten fullt implementert
    Trenger css hjelp igjen

---
+++ js/modules/MenuItems.js
@@ -0,0 +1,18 @@
+export default class MenuItems {
+    apply = (name, ingredients, inPrice, outPrice, profit) => {
+
+        let addPizza = `
+            <tr>
+                <td>${name}</td>
+                <td>${ingredients}</td>
+                <td>${inPrice}</td>
+                <td>${outPrice}</td>
+                <td>${profit}</td>
+                <td><img src="./images/stop-sign.png" width="24px" height="24px"></td>
+                <td><img src="./images/triple_dots.png" width="24px" height="24px"></td>
+            </tr>
+        `;
+
+        document.getElementById("menu_body").innerHTML += addPizza;
+    };
+}
