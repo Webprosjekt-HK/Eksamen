c444c1d52a90fd9b0b4a46337b29a7e741a912a1
Christian Gregersen <chgr007@student.kristiania.no>
2021-05-29 11:45:06 +0200

    admin page markup ish done

--- js/modules/MakeAdminPanels.js
+++ js/modules/MakeAdminPanels.js
@@ -6,23 +6,55 @@ export default class MakeAdminPanels {
     getPanel = () => {
         const containerElement = document.createElement("div");
         const departmentCollection = new DepartmentCollection();
-
-        containerElement.innerHTML = html` <div class="columns is-multiline">
-            <div class="column">
+        let checkBoxes = departmentCollection
+            .fetchDepartments()
+            .map((e) => {
+                return (
+                    '<div class="panel-block"><div class="control"><p class="subtitle is-5">Ansatt ved: </p><input type="checkbox" name="' +
+                    e.name +
+                    '" value="' +
+                    e.id +
+                    '"><label class="checkbox" for="' +
+                    e.name +
+                    '">' +
+                    e.name +
+                    '</label></div><div class="control"><p>Er leder?</span><br /><input type="radio" value="true" name="' +
+                    e.id +
+                    '" ><label for="' +
+                    e.id +
+                    '">Ja</label>' +
+                    '</label><input type="radio" value="false" name="' +
+                    e.id +
+                    '"><label for="' +
+                    e.id +
+                    '">Nei</label></div></div>'
+                );
+            }).join(" ");
+            console.log(checkBoxes);
+        containerElement.innerHTML = ` 
+        <div class="panel">
+            <p class="panel-heading">
+                Legg til ansatt
+            </p>
+            <div class="panel-block">
                 <div class="control">
                     <input
                         class="input is-focuesed first-name"
                         type="text"
                         placeholder="Fornavn"
-                    />
+                    >
                 </div>
+            </div>
+            <div class="panel-block">
                 <div class="control">
                     <input
                         class="input last-name"
                         type="text"
                         placeholder="etternavn"
-                    />
+                    >
                 </div>
+            </div>
+            <div class="panel-block">
                 <div class="control">
                     <input
                         class="input birth-date"
@@ -30,6 +62,8 @@ export default class MakeAdminPanels {
                         placeholder="YYYY-MM-DD"
                     />
                 </div>
+            </div>
+            <div class="panel-block">
                 <div class="control">
                     <textarea
                         class="input address"
@@ -39,33 +73,8 @@ export default class MakeAdminPanels {
                     ></textarea>
                 </div>
             </div>
-            <div class="column">
-                ${departmentCollection
-                    .fetchDepartments()
-                    .map((e) => {
-                        return (
-                            '<div class="control"><input type="checkbox" name="' +
-                            e.name +
-                            '" value="' +
-                            e.id +
-                            '"><label class="checkbox" for="' +
-                            e.name +
-                            '">' +
-                            e.name +
-                            '</label><p>Er leder?</span><br /><input type="radio" value="true" name="' +
-                            e.id +
-                            '" /><label for="' +
-                            e.id +
-                            '">Ja</label></div>' +
-                            '</label><input type="radio" value="false" name="' +
-                            e.id +
-                            '"/><label for="' +
-                            e.id +
-                            '">Nei</label></div>'
-                        );
-                    })
-                    .join(" ")}
-            </div>
+            ${checkBoxes}
+            <div class="panel-block"><button class="button">Lagre</button> </div>
         </div>`;
         return containerElement;
     };
